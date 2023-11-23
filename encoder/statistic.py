46be62f2de2e3323715611ca0921837fd3b2a38c
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2023-11-24 00:09:32 +0200

    Rewrote code for modularity and readability.

--- encoder/statistic.py
+++ encoder/statistic.py
@@ -3,14 +3,30 @@ import pandas as pd
 import scipy
 import numpy as np
 
+
+def read_tokens(file):
+    with open(file, "r") as file:
+        tokens = [token.strip() for token in file]
+        return tokens
+
+
 def count_tokens(file, tokens):
     with open(file, "r") as data:
         text = data.read()
         return {token: len(re.findall(token, text)) for token in tokens}
 
 
-tokens = ["li", "add", "lb", "beqz", "addi", "j", "mv", "ret", "sb", "bge", "sd", "mv", "call", "srai", "sub", "ld", "slli", "lw", "ble", "bnez", "fld", "fsw", "la", "bgt", "flw", "fadd.s", "fmul.s", "fmul.s.x", "fmv.s", "flt.s", "fgt.s", "fsqrt.d", "fadd.d", "fmul.d", "fsub.d"]
-token_counts = pd.DataFrame(count_tokens("raw_data.txt", tokens), index=[0])
-pk = np.asarray(token_counts.values).reshape(34) / np.asarray(token_counts.values).sum()
-entropy = scipy.stats.entropy(pk, base=2)
+def compute_appearence_probabilities(tokens_counts):
+    token_counts = pd.DataFrame(tokens_counts, index=[0])
+    return np.asarray(token_counts.values).reshape(len(tokens_counts)) / np.asarray(token_counts.values).sum()
+
+
+def compute_appearence_entropy(probs):
+    return scipy.stats.entropy(probs, base=2)
+
+
+tokens = read_tokens("instructions.txt")
+tokens_counts = count_tokens("raw_data.txt", tokens)
+tokens_probs = compute_appearence_probabilities(tokens_counts)
+entropy = compute_appearence_entropy(tokens_probs)
 print(entropy)
