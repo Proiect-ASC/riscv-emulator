f9a678dc5a62d32c9277d5cdd750821df8f3d49e
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2023-12-08 19:02:27 +0200

    Modularized encoding scheme.

--- encoder/encode.py
+++ encoder/encode.py
@@ -14,15 +14,21 @@ def encoding_efficiency_test(code_table, tokens_counts, entropy):
     return entropy / average_encoding_lenght * 100, average_encoding_lenght
 
 
-tokens = read_tokens("instructions.txt")
-tokens_counts = count_tokens("raw_data.txt", tokens)
-tokens_probs = compute_appearence_probabilities(tokens_counts)
-entropy = compute_appearence_entropy(tokens_probs)
+def encode_tokens(tokens_file, raw_data_file, tree_output_file, table_output_file):
+    tokens = read_tokens(tokens_file)
+    tokens_counts = count_tokens(raw_data_file, tokens)
+    tokens_probs = compute_appearence_probabilities(tokens_counts)
+    entropy = compute_appearence_entropy(tokens_probs)
 
-tokens_counts_nodes_pq = PriorityQueue([Node(value=[v, k]) for k, v in tokens_counts.items()])
+    tokens_counts_nodes_pq = PriorityQueue([Node(value=[v, k]) for k, v in tokens_counts.items()])
 
-huffman_tree, code_table, node_count = build_huffman_tree(tokens_counts_nodes_pq)
-print(f"encoding eff. is {encoding_efficiency_test(code_table, tokens_counts, entropy)}, code table is:\n{code_table}")
+    huffman_tree, code_table, node_count = build_huffman_tree(tokens_counts_nodes_pq)
+    print(
+        f"encoding eff. is {encoding_efficiency_test(code_table, tokens_counts, entropy)}, code table is:\n{code_table}")
 
-serialize_huffman_tree(huffman_tree, "serialized_huffman_tree.txt", node_count)
-serialize_code_table(code_table, "serialized_code_table.txt")
+    serialize_huffman_tree(huffman_tree, tree_output_file, node_count)
+    serialize_code_table(code_table, table_output_file)
+
+
+encode_tokens("instructions.txt", "raw_data.txt", "instructions_huffman_tree.txt", "instructions_code_table.txt")
+encode_tokens("registers.txt", "raw_data.txt", "registers_huffman_tree.txt", "registers_code_table.txt")
