10425f5c220d52256394884f19c0416b1928d3da
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2023-12-26 21:29:55 +0200

    Updated binaries and implemented the instructions needed for the first example.

--- executor/include/processor.h
+++ executor/include/processor.h
@@ -3,10 +3,12 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "treeloader.h"
 
 #define RAM_SIZE (8 * 1024)
 #define INT_REG_COUNT 32
 #define FLOAT_REG_COUNT 32
+#define ADDRESS_LENGHT 16
 
 typedef struct binary {
     uint8_t remainder;
@@ -21,9 +23,12 @@ typedef struct processor_t {
     float float_registers[FLOAT_REG_COUNT];
 } processor_t;
 
-void save_state(processor_t *proc, char *state_file_handle);
-void load_state(processor_t *proc, char *state_file_handle);
-binary load_executable(char *file_handle);
-void run(processor_t *proc, binary *program);
+void save_state(const processor_t *proc, const char *state_file_handle);
+void load_state(processor_t *proc, const char *state_file_handle);
+binary load_executable(const char *file_handle);
+extern inline uint8_t get_register(processor_t *proc, const huffman_tree *register_tree);
+extern inline uint16_t get_address(processor_t *proc);
+extern inline int get_int_immediate(processor_t *proc, uint8_t length);
+void run(processor_t *proc, const binary *program);
 
 #endif //EXECUTOR_PROCESSOR_H
