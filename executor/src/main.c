ab6cb32c2cad87eb0937237f3f9267acf9cc7d82
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2023-12-29 23:52:50 +0200

    Timed first example.

--- executor/src/main.c
+++ executor/src/main.c
@@ -1,16 +1,24 @@
 #include "processor.h"
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 void ex_1(processor_t *proc0) {
     char *str = "tibi o suge rau";
+    clock_t begin_dirty = clock();
     proc0->int_registers[22] = 192;
     assign_task(proc0, "../example_binaries/1.txt");
     memcpy(proc0->assigned_task.content + 24, str, strlen(str) + 1);
     save_state(proc0, "../state_files/1.in");
+    clock_t begin_clean = clock();
     run(proc0);
+    clock_t end_clean = clock();
+    double time_spent_clean = (double)(end_clean - begin_clean) / CLOCKS_PER_SEC;
     save_state(proc0, "../state_files/1.out");
-    printf("read file, program ends after %hu bits, answer: %d", proc0->assigned_task.program_end + 1, proc0->int_registers[22]);
+    clock_t end_dirty = clock();
+    double time_spent_dirty = (double)(end_dirty - begin_dirty) / CLOCKS_PER_SEC;
+    printf("read file, program ends after %hu bits, answer: %d\n", proc0->assigned_task.program_end + 1, proc0->int_registers[22]);
+    printf("took %f to run, %f to setup, total %f (all times in seconds)", time_spent_clean, time_spent_dirty - time_spent_clean, time_spent_dirty);
 }
 
 int main() {
