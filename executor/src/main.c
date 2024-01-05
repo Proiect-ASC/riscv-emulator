9332c71a591fe8c983692529e618da20b81a4ada
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-01-05 13:39:44 +0200

    Fixed first example string and added second example.

--- executor/src/main.c
+++ executor/src/main.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 void ex_1(processor_t *proc0) {
-    char *str = "tibi o suge rau";
+    char *str = "ana are mere";
     clock_t begin_dirty = clock();
     proc0->int_registers[22] = 192;
     assign_task(proc0, "../example_binaries/1.txt");
@@ -21,9 +21,28 @@ void ex_1(processor_t *proc0) {
     printf("took %f to run, %f to setup, total %f (all times in seconds)", time_spent_clean, time_spent_dirty - time_spent_clean, time_spent_dirty);
 }
 
+void ex_2(processor_t *proc0) {
+    char *str = "ana are mere";
+    clock_t begin_dirty = clock();
+    proc0->int_registers[22] = 192 + strlen(str) + 1;
+    proc0->int_registers[23] = 192;
+    assign_task(proc0, "../example_binaries/2.txt");
+    memcpy(proc0->assigned_task.content + 24, str, strlen(str) + 1);
+    save_state(proc0, "../state_files/2.in");
+    clock_t begin_clean = clock();
+    run(proc0);
+    clock_t end_clean = clock();
+    double time_spent_clean = (double)(end_clean - begin_clean) / CLOCKS_PER_SEC;
+    save_state(proc0, "../state_files/2.out");
+    clock_t end_dirty = clock();
+    double time_spent_dirty = (double)(end_dirty - begin_dirty) / CLOCKS_PER_SEC;
+    printf("read file, program ends after %hu bits, answer: %s\n", proc0->assigned_task.program_end + 1, &proc0->ram[24 + strlen(str) + 1]);
+    printf("took %f to run, %f to setup, total %f (all times in seconds)", time_spent_clean, time_spent_dirty - time_spent_clean, time_spent_dirty);
+}
+
 int main() {
     processor_t proc0;
-    ex_1(&proc0);
+    ex_2(&proc0);
 
     return 0;
 }
