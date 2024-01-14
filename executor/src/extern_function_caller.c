8a184edf8e956a393d9250d95680bb31ed69e2ef
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2024-01-14 13:24:36 +0200

    Created README file, updated a header to use the register enum.

--- executor/src/extern_function_caller.c
+++ executor/src/extern_function_caller.c
@@ -3,7 +3,7 @@
 void dispatch_call(char *func_name, processor_t *proc) {
     // append the corect call procedure to the if/else chain
     if (strcmp("cfunc", func_name) == 0) {
-        proc->int_registers[22] = cfunc(proc->int_registers[22], proc->int_registers[23], proc->int_registers[24]);
+        proc->int_registers[A0] = cfunc(proc->int_registers[A0], proc->int_registers[A1], proc->int_registers[A2]);
     } else {
         fprintf(stderr, "[ERROR] Could not find %s in library, terminating program...", func_name);
         exit(1);
